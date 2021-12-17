567b6105e7d1924679c56a131a68cf8a0fa295ed
nguyenhuahuy24 <huahuy1113@gmail.com>
2021-12-17 13:29:28 +0700

    swipeSlide 17/12/2021

---
+++ src/screens/home/Home/SwipeSlide.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  Image,
+} from 'react-native';
+const images =[        
+        require('../../../image/home/3.png'),
+        require('../../../image/home/1.png'),
+        require('../../../image/home/4.png'),
+        require('../../../image/home/14.jpg')
+      ];
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+export default class SwipeSlide extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+            imgActive:0,
+        }
+        
+    }
+    onChange =(nativeEvent)=>{
+        if(nativeEvent){
+            const slide =Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
+            if(slide != this.state.imgActive){
+                this.setState({imgActive:slide});
+            }
+        }
+    }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.wrap}>
+            <ScrollView
+                onScroll={({nativeEvent}) => this.onChange(nativeEvent)}
+                showsHorizontalScrollIndicator={false}
+                pagingEnabled
+                horizontal
+                style={styles.wrap}
+            >
+            {
+                images.map((e,index)=>
+                    <Image
+                        key={e}
+                        resizeMode="stretch"
+                        style={styles.wrap}
+                        source={e}
+                    />
+                )
+            }
+
+            </ScrollView>
+            <View style={styles.wrapDot}>
+                {
+                    images.map((e,index)=>
+                        <Text
+                            key={e}
+                            style={this.state.imgActive==index ? styles.dotActive: styles.dot}
+                        >
+                        ●
+                        </Text>
+                    )
+                }
+            </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    wrap:{
+        width: WIDTH,
+        height: HEIGHT*0.3,
+
+    },
+    wrapDot:{
+        position:'absolute',
+        bottom:0,
+        flexDirection:'row',
+        alignSelf:'center'
+    },
+    dotActive:{
+        margin:3,
+        color:'black'
+    },
+    dot:{
+        margin:3,
+        color:'white'
+    }
+})
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -3,15 +3,15 @@ import {
   View,
   Text,
 } from 'react-native';
+import SwipeSlide from "./Home/SwipeSlide";
 
 export default class HomeScreen extends React.Component {
 
   render() {
     return (
       <View style={{flex: 1,backgroundColor:'#e5e5e5',alignItems:'center',justifyContent:'center'}}>
-        <Text>
-            Trang chủ
-        </Text>
+        <SwipeSlide />
+        
       </View>
     );
   }
