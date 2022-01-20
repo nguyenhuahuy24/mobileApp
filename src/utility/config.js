6ee81fd4ce771f86117aa4adcf14237472b99118
truong99 <truongnguyen30999@gmail.com>
2022-01-20 19:42:47 +0700

    update

--- src/utility/config.js
+++ src/utility/config.js
@@ -1,7 +1,7 @@
 // export const url_image = "http://3.84.7.130:8080"
 // export const URL = "http://3.84.7.130:8080/api/mobile"
-export const url_image = "http://3.95.159.254:8080"
-export const URL = "http://3.95.159.254:8080/api/mobile"
+export const url_image = "http://3.89.142.152:8080"
+export const URL = "http://3.89.142.152:8080/api/mobile"
 
 export const dataStatus = {
     NONE: 'NONE',
